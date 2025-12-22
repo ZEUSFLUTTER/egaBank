54fedd8a69fef0c6bf3d8a2f8563d04aee320c71
ZEUSFLUTTER <dogblakleist04@gmail.com>
2025-12-23 00:32:16 +0100

    -fit :  ce commit vient clore l'implementation des service sur le compte

--- frontEgaBank/src/app/core/services/compte.service.ts
+++ frontEgaBank/src/app/core/services/compte.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@angular/core';
+// src/app/core/services/compte.service.ts
+
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { Compte} from '../models/comptes';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Compte {
-  
+export class CompteService {
+
+  constructor(private httpClient:HttpClient){}
+
+  private readonly API_URL = `${environment.apiUrl}/${environment.prefix}/comptes`;
+
+  postCompte(compte: Compte): Observable<Compte> {
+    return this.httpClient.post<Compte>(this.API_URL, compte);
+  }
+
+  getCompte(type: String): Observable<Compte[]> {
+    return this.httpClient.get<Compte[]>(`${this.API_URL}/type/${type}`);
+  }
+
+  getOneNumCompte(numCompte : string, type : string) : Observable<Compte>{
+    return this.httpClient.get<Compte>(`${this.API_URL}/${numCompte}/${type}`)
+  }
+
+  onActivateCompte(numCompte : string) : Observable<boolean>{
+    return this.httpClient.put<boolean>(`${this.API_URL}/acitve/${numCompte}`,{})
+  }
+
+  onSuspendedCompte(numCompte : string) : Observable<boolean>{
+    return this.httpClient.put<boolean>(`${this.API_URL}/suspendre/${numCompte}`,{})
+  }
 }
